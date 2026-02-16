728b976f2ce405cb12df4866a4f169fb6f86dd4b
Sreejitaxx <sreejitadas2005@gmail.com>
2026-02-16 11:04:42 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,16 @@
+/*
+*@author Dev
+* @Version 1.0
+*
+ */
+
+
+import java.util.*;
+public class PalindromeCheckerApp {
+    public static void main(String args[]) {
+        System.out.println("Welcome to Palindrome Checker Management System");
+        System.out.println("Version : 1,0");
+        System.out.println("System initialised successfully");
+    }
+}
+
