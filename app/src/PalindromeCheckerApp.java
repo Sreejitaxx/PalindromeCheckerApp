5f3647de2c87a8d65c046d918121084183a7a6f8
Sreejitaxx <sreejitadas2005@gmail.com>
2026-03-02 09:27:52 +0530

    Sreejita added usecase 2

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,8 +1,9 @@
-//version 1.0
+//version 2.0
 //author Sreejita
-//useCae 1: Welcome Page
+//useCae 2:
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         System.out.println("Welcome to Palindrome Checker App Management System");
+        System.out.println("Modify this logic to usecase 2 ");
     }
 }
