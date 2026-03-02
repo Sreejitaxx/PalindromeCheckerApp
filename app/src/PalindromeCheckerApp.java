9a463fad0d33225df9179300aaf066fa06792a22
Sreejitaxx <sreejitadas2005@gmail.com>
2026-03-02 09:52:40 +0530

    Sreejita added UC-4

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,23 +1,21 @@
 import java.util.*;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            System.out.print("Enter a string: ");
-            String original = sc.nextLine();
 
-            String reversed = "";
-
-            for (int i = original.length() - 1; i >= 0; i--) {
-                reversed = reversed + original.charAt(i);
+                String input = "radar";
+                char[] charArray = input.toCharArray();
+                int left = 0;
+                int right = charArray.length - 1;
+                boolean isPalindrome = true;
+                while (left < right) {
+                    if (charArray[left] != charArray[right]) {
+                        isPalindrome = false;
+                        break;
+                    }
+                    left++;
+                    right--;
+                }
+                System.out.println("input: " + input);
+                System.out.println("is palindrome?: " + isPalindrome);
             }
-
-            if (original.equals(reversed)) {
-                System.out.println("The string is a palindrome.");
-            } else {
-                System.out.println("The string is not a palindrome.");
-            }
-
-            sc.close();
         }
-}
-
